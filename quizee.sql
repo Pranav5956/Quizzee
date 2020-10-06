2628eae1d140db1f253bbede3c30614adbb8ac0f
Pranav Balaji <pranavbalaji01@gmail.com>
2020-10-06 09:02:37 +0530

    SQL updation

--- quizee.sql
+++ quizee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 03, 2020 at 11:54 AM
+-- Generation Time: Oct 06, 2020 at 05:31 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -35,17 +35,20 @@ CREATE TABLE `feedback` (
   `feedback_text` longtext DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `feedback`
+-- Table structure for table `groups`
 --
 
-INSERT INTO `feedback` (`fid`, `uid`, `qnid`, `attempt_no`, `feedback_text`) VALUES
-(5, 2, 141, 1, 'Only one difference provided'),
-(6, 2, 141, 2, 'Excellent answer!'),
-(7, 2, 142, 2, 'Good'),
-(8, 2, 143, 2, 'Good confidence'),
-(9, 5, 211, 1, 'Thank you Pranav Balaji for your feedback!'),
-(10, 5, 212, 1, 'Thank you');
+CREATE TABLE `groups` (
+  `gid` int(11) NOT NULL,
+  `ugid` varchar(35) NOT NULL,
+  `gname` varchar(20) NOT NULL,
+  `gdesc` longtext DEFAULT NULL,
+  `create_time` longtext NOT NULL,
+  `creator` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -67,24 +70,35 @@ CREATE TABLE `options` (
 --
 
 INSERT INTO `options` (`oid`, `option_number`, `description`, `isanswer`, `weightage`, `qnid`) VALUES
-(299, 0, NULL, 0, 4, 141),
-(300, 1, 'True', 0, 0, 142),
-(301, 2, 'False', 1, 1, 142),
-(302, 1, 'Chimpanzee', 1, 1, 143),
-(303, 2, 'Humans', 1, 1, 143),
-(304, 3, 'Giraffe', 0, 0, 143),
-(305, 4, 'Orangutans', 1, 1, 143),
-(306, 5, 'Humming Bird', 0, 0, 143),
-(307, 6, 'Platypus', 1, 1, 143),
-(308, 1, 'Orangutan', 0, 0, 144),
-(309, 2, 'Platypus', 1, 1, 144),
-(310, 3, 'Rats', 0, 0, 144),
-(311, 4, 'Humans', 0, 0, 144),
-(408, 0, NULL, 0, 0, 211),
-(409, 1, 'Good', 0, 0, 212),
-(410, 2, 'Bad', 0, 0, 212),
-(411, 3, 'Not bad', 0, 0, 212),
-(412, 4, 'OK', 0, 0, 212);
+(942, 0, NULL, 0, 0, 396),
+(1067, 1, '\'The Shining\' by Stephen King', 0, 0, 439),
+(1068, 2, '\'Oh! The Places You\'ll Go\' by Dr. Seuss', 1, 2, 439),
+(1069, 3, '\'Little Women\' by Louisa May Alcott', 0, 0, 439),
+(1070, 4, '\'Love You Forever\' by Robert Munsch', 0, 0, 439),
+(1071, 1, 'Aunt Lilian', 0, 0, 440),
+(1072, 2, 'Aunt Marilyn', 1, 2, 440),
+(1073, 3, 'Aunt Iris', 0, 0, 440),
+(1074, 4, 'Aunt Murial', 0, 0, 440),
+(1075, 0, NULL, 0, 2, 441),
+(1076, 0, NULL, 0, 2, 442),
+(1077, 1, 'Pugsy', 0, 0, 443),
+(1078, 2, 'Birdsy', 0, 0, 443),
+(1079, 3, 'Hugsy', 1, 2, 443),
+(1080, 1, 'James', 0, 0, 444),
+(1081, 2, 'Janet', 0, 0, 444),
+(1082, 3, 'Jack', 1, 2, 444),
+(1083, 4, 'Judy', 1, 2, 444),
+(1084, 5, 'John', 0, 0, 444),
+(1085, 6, 'Joan', 0, 0, 444),
+(1086, 1, 'Phoebe', 1, 2, 445),
+(1087, 2, 'Chandler', 1, 2, 445),
+(1088, 3, 'Joey', 0, 0, 445),
+(1089, 4, 'Monica', 0, 0, 445),
+(1090, 0, NULL, 0, 2, 446),
+(1091, 1, 'True', 1, 2, 447),
+(1092, 2, 'False', 0, 0, 447),
+(1093, 0, NULL, 0, 3, 448),
+(1097, 0, NULL, 0, 1, 451);
 
 -- --------------------------------------------------------
 
@@ -105,12 +119,18 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`qnid`, `question_number`, `description`, `type`, `qid`) VALUES
-(141, 1, 'Elucidate the differences between a lion and a tiger.', 'D', 72),
-(142, 2, 'Do whales have teeth?', 'TF', 72),
-(143, 3, 'Pick out the mammals from the following animals.', 'MCMQ', 72),
-(144, 4, 'Which mammal out of the following lays eggs?', 'MCQ', 72),
-(211, 1, 'Please provide feedback on the application and it\'s features.', 'D', 110),
-(212, 2, 'How would you describe the application\'s user interface?', 'MCQ', 110);
+(396, 1, 'Please provide feedback on the application and it\'s features.', 'D', 153),
+(439, 1, 'What book does Joey get Rachel for her birthday?', 'MCQ', 163),
+(440, 2, 'Which aunt does cousin Cassie look like?', 'MCQ', 163),
+(441, 3, 'Complete Joey\'s quote - \"Ohh, sorry. I hear \'divorce\', I immediately go to __________________.', 'D', 163),
+(442, 4, 'What was the name of the orthodontist Rachel was meant to marry in the pilot episode?', 'D', 163),
+(443, 5, 'What is the name of Joey\'s stuffed penguin?', 'MCQ', 163),
+(444, 6, 'What are Ross and Monica\'s parent\'s names?', 'MCMQ', 163),
+(445, 7, 'F.R.I.E.N.D.S was originally planned to have 4 \'friends\'. Which of the following were initially planned to be support roles?', 'MCMQ', 163),
+(446, 8, 'The names of all six friends were inspired by characters from which American Television series?', 'D', 163),
+(447, 9, 'David Schwimmer didn\'t have to audition for the character of \'Ross\'.', 'TF', 163),
+(448, 10, 'State the changes of Monica\'s, Chandler and Joey\'s apartment number and also the reason for such a change.', 'D', 163),
+(451, 1, 'Hello', 'D', 166);
 
 -- --------------------------------------------------------
 
@@ -133,8 +153,31 @@ CREATE TABLE `quizzes` (
 --
 
 INSERT INTO `quizzes` (`qid`, `uqid`, `qname`, `type`, `code`, `create_time`, `uid`) VALUES
-(72, 'Qc859c51b9c89d0eb551f0e5aad7f7801', 'Biology Quiz - 1', 'C', 'fea63cd0', '1598885783', 5),
-(110, 'Qaf9bd8132ff30b2ec4318b1173b06f23', 'General Survey', 'O', NULL, '1599074136', 2);
+(153, 'Q015bafb41071d3bc6433372717aeee4e', 'General Survey', 'O', NULL, '1599227951', 2),
+(163, 'Q71d617ce9eb9da47a00630be5b8a43b0', 'F.R.I.E.N.D.S Quiz', 'O', NULL, '1599460855', 5),
+(166, 'Q44d1b5417b7e8ddfd2d5ac73099fe43c', 'Quiz1', 'C', 'a0cd64a2', '1601355787', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_group`
+--
+
+CREATE TABLE `quiz_group` (
+  `gid` int(11) NOT NULL,
+  `qid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz_tag`
+--
+
+CREATE TABLE `quiz_tag` (
+  `qid` int(11) NOT NULL,
+  `tid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -159,22 +202,29 @@ CREATE TABLE `responses` (
 --
 
 INSERT INTO `responses` (`resp_id`, `attempt_no`, `uid`, `qid`, `qnid`, `oid`, `response`, `weightage`, `attempt_time`) VALUES
-(109, 1, 2, 72, 142, 301, NULL, 1, '1599029486'),
-(110, 1, 2, 72, 141, 299, 'Lions dont have stripes while tigers have black stripes', 1, '1599029486'),
-(111, 1, 2, 72, 144, 309, NULL, 1, '1599029486'),
-(112, 1, 2, 72, 143, 307, NULL, 1, '1599029486'),
-(113, 1, 2, 72, 143, 303, NULL, 1, '1599029486'),
-(114, 1, 2, 72, 143, 306, NULL, 0, '1599029486'),
-(115, 1, 2, 72, 143, 302, NULL, 1, '1599029486'),
-(116, 2, 2, 72, 141, 299, '1. Lion has a mane while a tiger doesn\'t.\r\n2. Lion does not have stripes while tiger has black stripes.\r\n3. Lion is the \'King of the Jungle\' but the tiger isn\'t.\r\n4. Tiger is the national animal of India, but the Lion isn\'t a national animal.\r\n', 4, '1599072662'),
-(117, 2, 2, 72, 142, 301, NULL, 1, '1599072662'),
-(118, 2, 2, 72, 143, 302, NULL, 1, '1599072662'),
-(119, 2, 2, 72, 143, 305, NULL, 1, '1599072662'),
-(120, 2, 2, 72, 143, 307, NULL, 1, '1599072662'),
-(121, 2, 2, 72, 143, 303, NULL, 1, '1599072662'),
-(122, 2, 2, 72, 144, 309, NULL, 1, '1599072662'),
-(123, 1, 5, 110, 211, 408, 'The application is very nice. The dark theme is fantastic!', 0, '1599074208'),
-(124, 1, 5, 110, 212, 409, NULL, 0, '1599074208');
+(135, 1, 2, 163, 444, 1082, NULL, 2, '1601437914'),
+(136, 1, 2, 163, 444, 1083, NULL, 2, '1601437914'),
+(137, 1, 2, 163, 446, 1090, '', 0, '1601437914'),
+(138, 1, 2, 163, 442, 1076, '', 0, '1601437914'),
+(139, 1, 2, 163, 448, 1093, '5 to 12', 1, '1601437914'),
+(140, 1, 2, 163, 439, 1068, NULL, 2, '1601437914'),
+(141, 1, 2, 163, 441, 1075, '', 0, '1601437914'),
+(142, 1, 2, 163, 447, 1091, NULL, 2, '1601437914'),
+(143, 1, 2, 163, 443, 1079, NULL, 2, '1601437914'),
+(144, 1, 2, 163, 440, 1071, NULL, 0, '1601437914'),
+(145, 1, 2, 163, 445, 1086, NULL, 2, '1601437914'),
+(146, 1, 2, 163, 445, 1087, NULL, 2, '1601437914');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE `tags` (
+  `tid` int(11) NOT NULL,
+  `tname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -210,6 +260,19 @@ INSERT INTO `users` (`uid`, `fname`, `lname`, `email`, `pwd`, `login`, `profile_
 (14, 'Bhavana', 'Ram', 'crypticlass01@gmail.com', 'fe1b25e00e547b3d652227c92004f8dc63542ec0c6b5ab8aab4b4070b2d87087', 'LOGIN', '../Resources/ProfilePictures/10e2fb4537576df60b3543c80dacdebb.jpg'),
 (15, 'a', 'a', 'a@bleh.com', 'f9b08c82a279463bbce0d836b4d47b9954338e055686f52f5b515f9fd843d644', 'LOGIN', '../Resources/ProfilePictures/f928a874e3ef14a6bd544312f58d839b.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_group`
+--
+
+CREATE TABLE `user_group` (
+  `qgid` int(11) NOT NULL,
+  `gid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `is_admin` enum('No','Yes') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -221,6 +284,13 @@ ALTER TABLE `feedback`
   ADD PRIMARY KEY (`fid`),
   ADD KEY `uid` (`uid`),
   ADD KEY `qnid` (`qnid`);
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`gid`),
+  ADD KEY `creator` (`creator`);
 
 --
 -- Indexes for table `options`
@@ -245,6 +315,20 @@ ALTER TABLE `quizzes`
   ADD KEY `uid` (`uid`);
 
 --
+-- Indexes for table `quiz_group`
+--
+ALTER TABLE `quiz_group`
+  ADD KEY `qid` (`qid`),
+  ADD KEY `gid` (`gid`);
+
+--
+-- Indexes for table `quiz_tag`
+--
+ALTER TABLE `quiz_tag`
+  ADD KEY `qid` (`qid`),
+  ADD KEY `tid` (`tid`);
+
+--
 -- Indexes for table `responses`
 --
 ALTER TABLE `responses`
@@ -255,10 +339,25 @@ ALTER TABLE `responses`
   ADD KEY `responses_ibfk_4` (`qid`);
 
 --
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`tid`),
+  ADD UNIQUE KEY `tname` (`tname`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `user_group`
+--
+ALTER TABLE `user_group`
+  ADD PRIMARY KEY (`qgid`),
+  ADD KEY `gid` (`gid`),
+  ADD KEY `uid` (`uid`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -268,37 +367,55 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `gid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `options`
 --
 ALTER TABLE `options`
-  MODIFY `oid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=413;
+  MODIFY `oid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1099;
 
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `qnid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=213;
+  MODIFY `qnid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=453;
 
 --
 -- AUTO_INCREMENT for table `quizzes`
 --
 ALTER TABLE `quizzes`
-  MODIFY `qid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+  MODIFY `qid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=168;
 
 --
 -- AUTO_INCREMENT for table `responses`
 --
 ALTER TABLE `responses`
-  MODIFY `resp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `resp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=147;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user_group`
+--
+ALTER TABLE `user_group`
+  MODIFY `qgid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
@@ -310,6 +427,12 @@ ALTER TABLE `users`
 ALTER TABLE `feedback`
   ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`),
   ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`qnid`) REFERENCES `questions` (`qnid`);
+
+--
+-- Constraints for table `groups`
+--
+ALTER TABLE `groups`
+  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `users` (`uid`);
 
 --
 -- Constraints for table `options`
@@ -330,6 +453,20 @@ ALTER TABLE `quizzes`
   ADD CONSTRAINT `quizzes_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
 
 --
+-- Constraints for table `quiz_group`
+--
+ALTER TABLE `quiz_group`
+  ADD CONSTRAINT `quiz_group_ibfk_2` FOREIGN KEY (`qid`) REFERENCES `quizzes` (`qid`),
+  ADD CONSTRAINT `quiz_group_ibfk_3` FOREIGN KEY (`gid`) REFERENCES `groups` (`gid`);
+
+--
+-- Constraints for table `quiz_tag`
+--
+ALTER TABLE `quiz_tag`
+  ADD CONSTRAINT `quiz_tag_ibfk_1` FOREIGN KEY (`qid`) REFERENCES `quizzes` (`qid`),
+  ADD CONSTRAINT `quiz_tag_ibfk_2` FOREIGN KEY (`tid`) REFERENCES `tags` (`tid`);
+
+--
 -- Constraints for table `responses`
 --
 ALTER TABLE `responses`
@@ -337,6 +474,13 @@ ALTER TABLE `responses`
   ADD CONSTRAINT `responses_ibfk_2` FOREIGN KEY (`oid`) REFERENCES `options` (`oid`),
   ADD CONSTRAINT `responses_ibfk_3` FOREIGN KEY (`qnid`) REFERENCES `questions` (`qnid`),
   ADD CONSTRAINT `responses_ibfk_4` FOREIGN KEY (`qid`) REFERENCES `quizzes` (`qid`);
+
+--
+-- Constraints for table `user_group`
+--
+ALTER TABLE `user_group`
+  ADD CONSTRAINT `user_group_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `groups` (`gid`),
+  ADD CONSTRAINT `user_group_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
